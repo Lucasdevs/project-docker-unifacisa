d8535eb19d57a20099b8e7ae5c0ba7a583bdb2e2
Lucas Costa Teotônio <69512117+Lucasdevs@users.noreply.github.com>
2025-09-28 19:29:29 -0300

    Delete listaDeTarefas-front-master directory

--- listaDeTarefas-front-master/src/app/app.config.ts
+++
@@ -1,6 +0,0 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(withInterceptorsFromDi())]
-};
