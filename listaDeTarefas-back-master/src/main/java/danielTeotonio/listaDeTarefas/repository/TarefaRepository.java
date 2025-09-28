9bb290a0f3db843f10cefbf6384333291df26d4b
Lucas Costa Teotônio <69512117+Lucasdevs@users.noreply.github.com>
2025-09-28 19:29:19 -0300

    Delete listaDeTarefas-back-master directory

--- listaDeTarefas-back-master/src/main/java/danielTeotonio/listaDeTarefas/repository/TarefaRepository.java
+++
@@ -1,7 +0,0 @@
-package danielTeotonio.listaDeTarefas.repository;
-
-import danielTeotonio.listaDeTarefas.domain.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
